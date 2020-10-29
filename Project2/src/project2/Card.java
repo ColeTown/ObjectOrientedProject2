00541f9d4753cb35da5e7976a9988fad5a01992c
ColeTown <33290898+ColeTown@users.noreply.github.com>
2020-10-29 16:09:45 -0500

    Fixed global fields and added toString Function

--- Project2/src/project2/Card.java
+++ Project2/src/project2/Card.java
@@ -2,14 +2,18 @@ package project2;
 
 public class Card {
     
-    private final int Color; // 0 = Blue, 1 = Yellow, 2 = Red, 3 = Green
-    private final int Number;
-    private final int cardType;
+    private int Color; // 0 = Blue, 1 = Yellow, 2 = Red, 3 = Green, -1 = Wild cards
+    private int Number; //-1 for action cards and wild cards
+    private int cardType; // 0 = Default, 1 = Skip, 2 = Draw Two, 3 = Reverse, 4 = Wild, 5 = Wild Draw 4
     
-    public Card(String color, int number, int cardType) {
+    public Card(int color, int number, int cardType) {
         this.Color = color;
         this.Number = number;
         this.cardType = cardType;
     }
     
+    @Override
+    public String toString() {
+        return "[" + Color + ", " + Number + ", " + cardType + "]";
+    }
 }
