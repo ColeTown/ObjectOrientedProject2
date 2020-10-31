70b5bebb73a698c505c00fb7acee948e08e14874
ColeTown <c4townsend@gmail.com>
2020-10-31 16:46:40 -0500

    Added comments

--- Project2/src/project2/Card.java
+++ Project2/src/project2/Card.java
@@ -1,46 +1,47 @@
 package project2;
 
 public class Card {
-    
+
     private int Color; // 0 = Blue, 1 = Yellow, 2 = Red, 3 = Green, -1 = Wild cards
     private int Number; //-1 for action cards and wild cards
     private int cardType; // 0 = Default, 1 = Skip, 2 = Draw Two, 3 = Reverse, 4 = Wild, 5 = Wild Draw 4
-    
+
     public Card(int color, int number, int cardType) {
         this.Color = color;
         this.Number = number;
         this.cardType = cardType;
     }
-    
+
     void setCardType(int cardType) {
         this.cardType = cardType;
     }//end setCardType
-    
+
     int getCardType() {
         return this.cardType;
     }//end getCardType
-    
+
     void setNumber(int number) {
         this.Number = number;
     }//end setNumber
-    
+
     int getNumber() {
         return this.Number;
     }//end getNumber
-    
+
     void setColor(int color) {
         this.Color = color;
     }//end setColor
-    
+
     int getColor() {
         return this.Color;
     }//end getColor
-    
+
     @Override
     public String toString() {
         return "[" + Color + ", " + Number + ", " + cardType + "]";
     }
-    
+
+    //compareTo is used for sorting the cards in the hand
     public int compareTo(Object o) {
         if (Color > ((Card) o).getColor()) {
             return 1;
@@ -51,6 +52,5 @@ public class Card {
         } else {
             return 0;
         }
-    }
+    }//end compareTo
 }
-
--- Project2/src/project2/Deck.java
+++ Project2/src/project2/Deck.java
@@ -3,41 +3,48 @@ package project2;
 import java.util.*;
 
 public class Deck {
-    
+
     public ArrayList<Card> Cards;
     private boolean hasActionCards;
-    
+
     public Deck() {
         this(1, true, true);
         this.Cards = new ArrayList();
     }
-    
+
     public Deck(int numberOfDecks, boolean shuffledTogether, boolean includeActionCards) {
         this.Cards = new ArrayList();
         this.hasActionCards = includeActionCards;
 
-        for (int i = 0; i < numberOfDecks; i++) { //Creates the Deck
-            for (int j = 1; j < 20; j++) { //adds two of each number 1-9 of each color and only 1 zero of each color
+        //Creates the deck out of the following preset, running multiple times based on "numberOfDecks"
+        for (int i = 0; i < numberOfDecks; i++) { 
+            
+            //adds two of each number 1-9 of each color and only 1 zero of each color
+            for (int j = 1; j < 20; j++) { 
                 Cards.add(new Card(0, j % 10, 0));
                 Cards.add(new Card(1, j % 10, 0));
                 Cards.add(new Card(2, j % 10, 0));
                 Cards.add(new Card(3, j % 10, 0));
             }
+            
             if (includeActionCards) {
-                for (int x = 0; x < 6; x++) { //will add two of each action card to the deck
+                //will add two of each action card to the deck
+                for (int x = 0; x < 6; x++) {
                     Cards.add(new Card(0, -1, 1 + x / 2));
                     Cards.add(new Card(1, -1, 1 + x / 2));
                     Cards.add(new Card(2, -1, 1 + x / 2));
                     Cards.add(new Card(3, -1, 1 + x / 2));
                 }
             }
+            
+            //adds 4 of each type of wild card
             for (int y = 0; y < 4; y++) {
                 Cards.add(new Card(-1, -1, 4));
                 Cards.add(new Card(-1, -1, 5));
             }
         }
 
-        //This is the logic used to shuffle the deck individually and together
+        //This is the logic used to shuffle the deck individually/together
         if (shuffledTogether) {
             Collections.shuffle(Cards);
         } else {
@@ -54,24 +61,23 @@ public class Deck {
                     Collections.shuffle(Cards.subList(singleDeckSize * 2, singleDeckSize * 3));
                 }
             }
-            
         }
-
     }
-    
+
+    //takes n cards from the deck and returns them as an array
     public Card[] takeCard(int n) {
-        if(Cards.size() < n) {
+        if (Cards.size() < n) {
             n = Cards.size();
         }
         Card[] temp = new Card[n];
-        for(int i = 0; i < n; i++) {
-         temp[i] = Cards.remove(0);
+        for (int i = 0; i < n; i++) {
+            temp[i] = Cards.remove(0);
         }
         return temp;
-    }
-    
+    }//end takeCard
+
+    //places a card back into the deck at the end
     public void returnCard(Card x) {
         Cards.add(x);
-    }
-    
+    }//end returnCard
 }
