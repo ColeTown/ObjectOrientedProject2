b4798370c248270dfeb9dd313c4611ee98200584
ColeTown <c4townsend@gmail.com>
2020-10-30 19:17:11 -0500

    Added SortCard Functionality

--- Project2/src/project2/Hand.java
+++ Project2/src/project2/Hand.java
@@ -2,22 +2,40 @@ package project2;
 
 public class Hand {
     
-    private Card[] CurrentHand = new Card[7];
+    private final int sizeOfHand = 7;
+    private Card[] CurrentHand = new Card[sizeOfHand];
     
     public Hand() {
         
     }
     
-    public void SortCards() {
-        //sort the array CurrentHand by given criteria
+    private void SortCards() {
+        int indexOfSmallest;
+        Card temp;
+        for(int i = 0; i < sizeOfHand; i++) {
+            indexOfSmallest = i;
+         for(int j = i+1; j < sizeOfHand; j++) {   
+          if(CurrentHand[indexOfSmallest].compareTo(CurrentHand[j]) == 1)   {
+              indexOfSmallest = j;
+          }
+         }
+         temp = CurrentHand[i];
+         CurrentHand[i] = CurrentHand[indexOfSmallest];
+         CurrentHand[indexOfSmallest] = temp;
+        }
     }
     
     public void setHand(Card[] newHand) {
         this.CurrentHand = newHand;
+        SortCards();
     }
     
     public Card[] getHand() {
         return this.CurrentHand;
+    }
+    
+    public int getHandSize() {
+        return this.sizeOfHand;
     }
     
 }
