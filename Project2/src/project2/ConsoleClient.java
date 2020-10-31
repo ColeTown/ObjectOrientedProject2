70a523b8e0182a1df3da7aac1e6c24668667374b
ColeTown <33290898+ColeTown@users.noreply.github.com>
2020-10-31 18:42:58 -0500

    Cole's branch (#10)
    * Coles-Branch (#8)
    
    * Cole's branch (#5)
    
    * Initialized the space in memory for the CurrentHand to have 7 indices
    
    * Built functionality for takeCard method. returns an array of n cards from the deck and added its use in Project2.java
    
    * Added SortCard Functionality
    
    * Added compareTo function to use when sorting cards
    
    * Added round counter to know what round we are on
    
    * Added Round Scene Functionality
    
    * Had to specify hand size
    
    * Fixed takeCard function and deck creation issue
    
    * Fixed Switch formatting
    
    * Line 62: changed (int j = 0; i < handSize; j++) to (int j = 0; j < handsize; j++)
    
    * implemented exercise counts
    
    * Added comments
    
    * Changed GUI format of showing exercises
    
    Co-authored-by: Cole Townsend <>
    
    * Fixed current and total reps
    
    Also fixed reverse card being put back into deck.
    
    * updated method names for current reps
    
    Co-authored-by: ZHUNT64 <61096461+ZHUNT64@users.noreply.github.com>
    
    * Brushed up finalScene and roundScene
    
    * removed unneeded variable
    
    * Made new client and renamed GUI client
    
    Co-authored-by: ZHUNT64 <61096461+ZHUNT64@users.noreply.github.com>
    Co-authored-by: c4tow <c4tow@MSI>

---
+++ Project2/src/project2/ConsoleClient.java
@@ -0,0 +1,10 @@
+package project2;
+
+
+public class ConsoleClient {
+    
+    public static void main(String args[]) {
+        
+    }
+    
+}
--- Project2/src/project2/GUIClient.java
+++ Project2/src/project2/GUIClient.java
@@ -185,3 +185,4 @@ public class GUIClient extends Application {
     }
 
 }
+
