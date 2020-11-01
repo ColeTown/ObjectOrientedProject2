def31c670351e1fc2b42e30513639a2caf12a7c2
ColeTown <33290898+ColeTown@users.noreply.github.com>
2020-11-01 16:08:07 -0600

    Cole's branch (#14)
    * updating branch (#13)
    
    * Cole's branch (#11)
    
    * Coles-Branch (#8)
    
    * Cole's branch (#5)
    
    * Initialized the space in memory for the CurrentHand to have 7 indices
    
    * Built functionality for takeCard method. returns an array of n cards from the deck and added its use in Project2.java
    
    * Added SortCard Functionality
    
    * Added compareTo function to use when sorting cards
    
    * Added round counter to know what round we are on
    
    * Added Round Scene Functionality
    
    * Had to specify hand size
    
    * Fixed takeCard function and deck creation issue
    
    * Fixed Switch formatting
    
    * Line 62: changed (int j = 0; i < handSize; j++) to (int j = 0; j < handsize; j++)
    
    * implemented exercise counts
    
    * Added comments
    
    * Changed GUI format of showing exercises
    
    Co-authored-by: Cole Townsend <>
    
    * Fixed current and total reps
    
    Also fixed reverse card being put back into deck.
    
    * updated method names for current reps
    
    Co-authored-by: ZHUNT64 <61096461+ZHUNT64@users.noreply.github.com>
    
    * Brushed up finalScene and roundScene
    
    * removed unneeded variable
    
    * Made new client and renamed GUI client
    
    * Added to ConsoleClient
    
    Co-authored-by: ZHUNT64 <61096461+ZHUNT64@users.noreply.github.com>
    Co-authored-by: c4tow <c4tow@MSI>
    
    * Updated all classes and added ConsoleClient
    
    * Added console ui for using program
    
    * updated ConsoleClient class uml
    
    * Actually set hand this time
    
    * Added more documentation
    
    * Added documentation
    
    * Added documentation
    
    * Added documentation
    
    * Added documentation
    
    * Added documentation
    
    Co-authored-by: ZHUNT64 <61096461+ZHUNT64@users.noreply.github.com>
    Co-authored-by: c4tow <c4tow@MSI>
    
    * tempString was being printed to console everytime it was updated. only needs to be printed once at the end
    
    * Added HTML File creation
    
    Co-authored-by: ZHUNT64 <61096461+ZHUNT64@users.noreply.github.com>
    Co-authored-by: c4tow <c4tow@MSI>

--- Project2/src/project2/ConsoleClient.java
+++ Project2/src/project2/ConsoleClient.java
@@ -4,6 +4,9 @@ package project2;
 they want to sort the decks individually or separate. Also allows the user to pick if they want action and wild cards in the workout.
 Displays the data to the console for the user to read and creates an html file after each round to see data for the round.*/
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 
@@ -18,8 +21,24 @@ public class ConsoleClient {
     private static int round = 1;
 
     
-    public static void main(String args[]) {
+    public static void main(String args[]) throws IOException {
+        
+
         Scanner s = new Scanner(System.in);
+        String fileNameDir = "";
+        System.out.println("Type where you would like to save your HTML file: (ex. C:\\Users\\MyName\\Documents) ");
+        fileNameDir += s.nextLine().trim();
+        if(fileNameDir.charAt(fileNameDir.length() - 1) != '\\') {
+            fileNameDir += "\\";
+        }
+        System.out.println("Type what you want the name of your file to be: (ex. filename.html) ");
+        fileNameDir += s.nextLine().trim();
+        
+        File html = new File(fileNameDir);
+        FileWriter writer = new FileWriter(fileNameDir);
+        writer.write("<!DOCTYPE html>\n<html>\n<body>");
+        
+        
         System.out.println("How many decks do you want: ");
         numberOfDecks = s.nextInt();
         System.out.println("Enter \"1\" to shuffle decks together and \"0\" to shuffle them seperatly: ");
@@ -28,39 +47,43 @@ public class ConsoleClient {
         includeActionCards = s.nextInt() == 1;
         deck = new Deck(numberOfDecks, shuffleTogether, includeActionCards);
         hand = new Hand();
-        
+        System.out.println();
 
        
         while(!deck.Cards.isEmpty()) {
-            System.out.println("Round " + round++);
+
+            System.out.println("Round " + round);
+            writer.write("Round " + round++);
+
             hand.setHand(deck.takeCard(hand.getNormalHandSize()));
             //one round takes place in one iteration of this loop
             String tempString = "Cards: ";
-            System.out.println(tempString);
             Card[] tempHand = hand.getHand();
             for (int i = 0; i < hand.getCurrentHandSize(); i++) {
                 Card tempCard = tempHand[i];
                 switch (tempCard.getColor()) {
-                    case -1: System.out.println(tempString += "Wild "); break;
-                    case 0: System.out.println(tempString += "Blue "); break;
-                    case 1: System.out.println(tempString += "Yellow "); break;
-                    case 2: System.out.println(tempString += "Red "); break;
-                    case 3: System.out.println(tempString += "Green "); break;
+                    case -1: tempString += "Wild "; break;
+                    case 0: tempString += "Blue "; break;
+                    case 1: tempString += "Yellow "; break;
+                    case 2: tempString += "Red "; break;
+                    case 3: tempString += "Green "; break;
                 }
                 if (tempCard.getNumber() != -1) {
                     tempString += tempCard.getNumber();
                 }
                 switch (tempCard.getCardType()) {
-                    case 1: System.out.println(tempString += "Skip"); break;
-                    case 2: System.out.println(tempString += "Draw Two"); break;
-                    case 3: System.out.println(tempString += "Reverse"); break;
-                    case 4: System.out.println(tempString += "Card"); break;
-                    case 5: System.out.println(tempString += "Draw Four"); break;
+                    case 1: tempString += "Skip"; break;
+                    case 2: tempString += "Draw Two"; break;
+                    case 3: tempString += "Reverse"; break;
+                    case 4: tempString += "Card"; break;
+                    case 5: tempString += "Draw Four"; break;
                 }
                 if (i != hand.getCurrentHandSize() - 1) {
-                    System.out.println(tempString += ", ");
+                    tempString += ", ";
                 }
             }
+            System.out.println(tempString);
+            writer.write("<p>" + tempString + "</p>");
             workout.calculateRound(hand.getHand(), hand.getCurrentHandSize());
 
             int pushups = workout.getCurrentPushReps();
@@ -69,27 +92,34 @@ public class ConsoleClient {
             int lunges = workout.getCurrentLungeReps();
             int burpees = workout.getCurrentBurpReps();
             
-            System.out.println();
-            System.out.println("Exercises: \n    " + pushups + " Push Ups\n    " + squats + " Squats\n    " + situps + " Situps\n    " + lunges + " Lunges\n    " + burpees + " Burpees");
-            System.out.println();
-            System.out.println(tempString = "Statistics: \n    ");
-            System.out.println();
+
+            tempString = "Exercises: \n    " + pushups + " Push Ups\n    " + squats + " Squats\n    " + situps + " Situps\n    " + lunges + " Lunges\n    " + burpees + " Burpees";
+            System.out.println(tempString);
+            writer.write("<p>" + tempString.replaceAll("\n","<br>") + "</p>");
+            tempString = "Statistics: \n    ";
             System.out.println(tempString += "Cards Left: " + deck.Cards.size());
-            System.out.println();
+            writer.write("<p>" + tempString.replaceAll("\n","<br>") + "<br><br></p>");
+            System.out.println("\n\nPress Enter to go to next round\n");
+            s.nextLine();
+            
+        }
+
         
-        System.out.println();
-        System.out.println("Workout Over");
-        System.out.println();
-        System.out.println("Statistics: \n   ");
-        System.out.println();
-        System.out.println("Total Skiped Reps: " + workout.getTotalSkips() + "\n\n   ");
-        System.out.println("Total Push Ups: " + workout.getTotalPushReps() + "\n   ");
-        System.out.println("Total Squats: " + workout.getTotalSquatReps() + "\n   ");
-        System.out.println("Total Situps: " + workout.getTotalSitReps() + "\n   ");
-        System.out.println("Total Lunges: " + workout.getTotalLungeReps() + "\n   ");
-        System.out.println("Total Burpees: " + workout.getTotalBurpReps() + "\n   ");
+        String tempString = "Workout Over\n";
+        tempString += "Statistics: \n   ";
+        tempString += "Total Skiped Reps: " + workout.getTotalSkips() + "\n\n   ";
+        tempString += "Total Push Ups: " + workout.getTotalPushReps() + "\n   ";
+        tempString += "Total Squats: " + workout.getTotalSquatReps() + "\n   ";
+        tempString += "Total Situps: " + workout.getTotalSitReps() + "\n   ";
+        tempString += "Total Lunges: " + workout.getTotalLungeReps() + "\n   ";
+        tempString += "Total Burpees: " + workout.getTotalBurpReps() + "\n   ";
+        System.out.println("\n" + tempString);
+        writer.write("<p>" + tempString.replaceAll("\n","<br>") + "</p>");
         
-    }//end main
-    
-}//end ConsoleClient
+        writer.write("</body>\n</html>");
+        writer.close();
+        
+        }//end main
+
+    }//end ConsoleClient
 
